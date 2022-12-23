d22a10e9d49b899dda8ebf5685d8c52fb97cf9da
CptMeow <mr.worapon@gmail.com>
2022-12-23 15:26:53 +0700

    Update Contract CODE

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -28,7 +28,7 @@ class ContractController extends Controller
             return Datatables::eloquent($records)
                 ->addIndexColumn()
                 ->addColumn('contract_name_output', function ($row) {
-                    $html = $row->contract_name;
+                    $html = '[' . $row->contract_number . '] ' . $row->contract_name;
                     $html .= '<br><span class="badge bg-info">' . Helper::date($row->contract_start_date) . '</span> -';
                     $html .= ' <span class="badge bg-info">' . Helper::date($row->contract_end_date) . '</span>';
 
